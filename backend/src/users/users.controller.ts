d4014521a6a44be9010a002636490447da405f6e
EstbanGa <estban1503@gmail.com>
2025-09-09 16:29:45 -0500

    Frontend implemented, MongoDB hasn’t needed changes, Sign-up functionality added in users.controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { RolesDecorator } from '../auth/decorators/roles.decorator';
@@ -6,6 +8,13 @@ import { Roles } from '../auth/roles';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
   @UseGuards(JwtAuthGuard, RolesGuard)
   @RolesDecorator(Roles.JUAN)
   @Get('protected')
